d1d6b791f7ff4eba65a6520a2d65512832b60f20
Jasper <jasper@algra.io>
2021-07-23 23:52:20 +0200

    vendor included

---
+++ inc/plugins/wp-less/vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8c06253710526e6fbb1c1bdff2b7cce554f30696',
+    'name' => 'oncletom/wp-less',
+  ),
+  'versions' => 
+  array (
+    'leafo/lessphp' => 
+    array (
+      'pretty_version' => 'v0.5.0',
+      'version' => '0.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f5a7f5545d2bcf4e9fad9a228c8ad89cc9aa283',
+    ),
+    'oncletom/wp-less' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8c06253710526e6fbb1c1bdff2b7cce554f30696',
+    ),
+    'oyejorge/less.php' => 
+    array (
+      'pretty_version' => 'v1.7.0.14',
+      'version' => '1.7.0.14',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '42925c5a01a07d67ca7e82dfc8fb31814d557bc9',
+    ),
+  ),
+);
